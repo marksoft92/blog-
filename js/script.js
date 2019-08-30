8f22487d7e36aa8adff7a8553f8e35b78ca518ac
Dawid BIeńkowski <marksoft1992@gmail.com>
2019-08-30 14:52:24 +0200

    clikedElement

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@
 
 
 function titleClickHandler(event) {
-    console.log(event);
+    const clickedElement = this;
     console.log('Link was clicked!');
 
-    /* remove class 'active' from all article links  */
+    /*[DONE] remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    /*[DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('article.active');
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+        console.log('zniklo');
+    }
 
     /* get 'href' attribute from the clicked link */
 
